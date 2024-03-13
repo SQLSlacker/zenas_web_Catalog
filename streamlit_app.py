93975411fcb747e8d0c6a6234ee858341be5c334
SQLSlacker <12687158+SQLSlacker@users.noreply.github.com>
2024-03-13 14:10:29 -0500

    Update streamlit_app.py
    testing half of final code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,19 @@
 # Import python packages
 import streamlit as st
 import snowflake.connector
+import pandas as pd
 
-st.title("Zena's Web Catalog Prototype")
-
+st.title('Zena\'s Amazing Athleisure Catalog')
+# connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
-CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+# put the dafta into a dataframe
+df = pd.DataFrame(my_catalog)
+# temp write the dataframe to the page so I Can see what I am working with
+# streamlit.write(df)
+# put the first column into a list
+color_list = df[0].values.tolist()
+# print(color_list)
