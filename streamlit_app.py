cdadeefd8748cf35ff4c85156a195cedd7f8cbca
SQLSlacker <12687158+SQLSlacker@users.noreply.github.com>
2024-03-13 14:22:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
 # Import python packages
-import streamlit
+import streamlit as st
 import snowflake.connector
 import pandas
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+cnx = st.connection("snowflake")
+session = cnx.session
+
+my_cur = cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
